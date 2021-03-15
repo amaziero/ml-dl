83537f306848ddc0578d5c6d4807af2bcb587623
amaziero <alison_lens@hotmail.com>
2021-03-15 20:46:13 -0300

    feat: one hot encoding the country collumn

--- src/2_section/code/data_preprocessing_tools/data_preprocessing_tools.py
+++ src/2_section/code/data_preprocessing_tools/data_preprocessing_tools.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 def importing_dataset_returning_xy():
 	dataset = pd.read_csv("../../data/Data.csv")
@@ -15,6 +17,29 @@ def replacing_missing_values_with_avarage(x):
 	x[:, 1:3] = impute.transform(x[:, 1:3])
 	return x
 
+def one_hot_enconding(x):
+	"""This function is responsable for encoding the colluns with str values
+	in this case onle the fisrt collumn is appliable
+
+	Args:
+			x (as array): the x array of and preprocessed dataset that will be latter
+			used to train an mld or something alike.
+
+		It returns an numpy array (x passed) with new the first's collumns encoded
+	"""	
+	ct = ColumnTransformer(
+		transformers = [('encoder',
+		OneHotEncoder(), [0])],
+		remainder = 'passthrough'
+	)
+
+	# forcing the return to be an numpy array after transformer to onehot encoding
+	x = np.array(ct.fit_transform(x))
+	
+	return x
+
 x, y = importing_dataset_returning_xy()
 x = replacing_missing_values_with_avarage(x)
+x = one_hot_enconding(x)
+
 print(x)
